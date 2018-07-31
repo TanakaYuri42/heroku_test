e41e4792d53c6074c027b172a6bc0b1e73a517ba
y-tanaka <y-tanaka@letsiwahana.ttcad.local>
2018-07-31 10:42:11 +0900

    Adding files.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
+'use strict'
+
+//const path = require('path')
+
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  // dev: {
+
+  // },
+  // build: {
+  //   // Paths
+  //   assetsRoot: path.resolve(__dirname, 'dist'),
+  //   assetsSubDirectory: 'static',
+  //   assetsPublicPath: '',
+  // }
 }
